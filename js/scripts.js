4d190ecb2347739f7195081933f303b42ebf1aec
donnymays <dtmays2@gmail.com>
2020-08-18 16:38:34 -0700

    continue js workings

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,33 @@ $(document).ready(function() {
     newArr = [formInt];
     for (let i=0; i < formInt;i += 1) {
       let newInt = (newArr[i]-1);
+      if (newInt === 0) {
+        break;
+      };
       newArr.push(newInt);
       console.log(newArr);
     }
     
-    // let intArray = [];
+    // let factorial = 1
+    // newArr.forEach(function(number){
+    //   factorial *= number;
+      
+   
 
-    // for (let i = formInt; i < intArray.length; i +=1) {
-    //   intArray.push(formInt[i])
-    //}
+      for (let i=1; i<newArr.length; i+=1) {
+        let facNum = newArr[0] * newArr[1];
+        let newFacNum = facNum
+        newFacNum *= newArr[2+i];
+        console.log(newFacNum);
+        
+
+      }
+      
+    
+    
+      
+  
+
   });
 });
 
