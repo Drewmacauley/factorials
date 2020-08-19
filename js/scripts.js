e5396599de615f87b7b43f827c262bdd62ed8e96
donnymays <dtmays2@gmail.com>
2020-08-19 08:03:45 -0700

    trying more js code

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,32 @@ $(document).ready(function() {
     event.preventDefault();
     const formInt = parseInt($("input#numberInput").val());
 
-    newArr = [formInt];
+    let newArr = [formInt];
     for (let i=0; i < formInt;i += 1) {
       let newInt = (newArr[i]-1);
       if (newInt === 0) {
         break;
       };
       newArr.push(newInt);
-      console.log(newArr);
+      // console.log(newArr);
     }
     
-    // let factorial = 1
-    // newArr.forEach(function(number){
-    //   factorial *= number;
-      
-   
-
-      for (let i=1; i<newArr.length; i+=1) {
-        let facNum = newArr[0] * newArr[1];
-        let newFacNum = 1
-        newFacNum *= newArr[2+i];
-        console.log(newFacNum);
-        
-
-      }
-      
     
-    
-      
-  
+    for (i = 0; i < newArr.length; i +=1) {
+      let newFacNum = 1;
+      newFacNum *= newArr[i];
+      console.log(newFacNum);
+    }
+    // for (let i = 1; i < newArr.length; i++) {
+    //   let newFacNum = newArr[i] * i;
+    //   // console.log(newFacNum);
+    // };
+
+      // for (let i=1; i<newArr.length; i+=1) {
+      //   let facNum = newArr[0] * newArr[1];
+      //   let newFacNum = facNum
+      //   newFacNum *= newArr[2+i];
+      //   console.log(newFacNum);
 
   });
 });
